76ee2c226296989a81cc529dfddbdf058ccaeeb1
chinh4thepro <chinh4thepro@gmail.com>
2024-03-06 18:03:30 -0500

    whats

--- home-manager/modules/fonts.nix
+++ home-manager/modules/fonts.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}: {
-  fonts.fontconfig.enable = true;
+  fonts = {
+    fontconfig.enable = true;
+  };
 
   home.packages = with pkgs; [
     rPackages.fontawesome
-    nerdfonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-emoji
+    (nerdfonts.override { fonts = [ "FantasqueSansM" ]; })
   ];
 }
-
