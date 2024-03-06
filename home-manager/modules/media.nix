25550c6c30f51d4a1ba3c9223a7331a0a34f5ebd
chinh4thepro <chinh4thepro@gmail.com>
2024-03-06 12:46:39 -0500

    a

--- home-manager/modules/media.nix
+++ home-manager/modules/media.nix
@@ -6,10 +6,8 @@
   ]
   ++ lib.optionals stdenv.isDarwin [
     iina
-    inputs.jerry.packages."x86_64-darwin".jerry
-    gnused
   ]
   ++ lib.optionals stdenv.isLinux [
-    inputs.jerry.packages."x86_64-linux".jerry
+    mpv
   ];
 }
