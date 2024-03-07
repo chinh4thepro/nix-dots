85b72cff473c41580977da331bfd7e4796f8474e
chinh4thepro <chinh4thepro@gmail.com>
2024-03-06 19:04:28 -0500

    guess no manga for me

--- home-manager/modules/media.nix
+++ home-manager/modules/media.nix
@@ -1,15 +1,14 @@
 {pkgs, ...}: {
   home.packages = with pkgs;
   [
-    nsxiv
-    feh
     mpv
     ani-cli
   ]
   ++ lib.optionals stdenv.isDarwin [
     iina
-    xquartz
   ]
   ++ lib.optionals stdenv.isLinux [
+    nsxiv
+    feh
   ];
 }
