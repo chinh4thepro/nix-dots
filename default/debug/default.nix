2189ac8c3d5a9a2fe671f70ff0a929984d1e990a
chinh4thepro <chinh4thepro@gmail.com>
2024-04-10 17:44:53 -0400

    i guess plasma 6 is unstable too *sigh*

--- default/debug/default.nix
+++ default/debug/default.nix
@@ -10,7 +10,7 @@
     xserver = {
       enable = true;
       displayManager.sddm.enable = true;
+      desktopManager.plasma5.enable = true;
     };
-    desktopManager.plasma6.enable = true;
   };
 }
