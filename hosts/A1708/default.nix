79cc041e2dbfe566ba1e8380f67bf2580312fa1a
chinh4thepro <chinh4thepro@gmail.com>
2024-04-10 12:42:22 -0400

    AAA

--- hosts/A1708/default.nix
+++ hosts/A1708/default.nix
@@ -6,9 +6,9 @@
 
   # Boot
   boot = {
-    systemd-boot.enable = true;
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
+      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       systemd-boot.configurationLimit = 10;
     };
