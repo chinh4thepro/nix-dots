3449283e9bf81533d2cad906827105917a5b1376
chinh4thepro <chinh4thepro@gmail.com>
2024-04-10 12:41:05 -0400

    heheh

--- hosts/A1708/default.nix
+++ hosts/A1708/default.nix
@@ -4,5 +4,31 @@
     ../../default/nixos
   ];
 
+  # Boot
+  boot.loader = {
+    systemd-boot.enable = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.configurationLimit = 10;
+    };
+  };
+
+  # Networking
+  networking = {
+    hostName = "A1708";
+    networkmanager.enable = true;
+  };
+
+  # Services
+  services = {
+    openssh.enable = true;
+  };
+
+  # Nix options
+  nix = {
+    distributedBuilds = true;
+  };
+
   system.stateVersion = "24.05";
 }
