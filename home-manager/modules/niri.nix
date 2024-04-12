ab2fc073a00ba590d68a8938d700d430a3a6c7d2
chinh4thepro <chinh4thepro@gmail.com>
2024-04-12 18:10:56 -0400

    niriiiiiiiiiiiiiiiiiiiiiii?

--- home-manager/modules/niri.nix
+++ home-manager/modules/niri.nix
@@ -17,8 +17,8 @@
 
         # Keybinds
         binds = {
-          "Mod+Shift+Slash".action.show-hotkey-overlay;
-          "Mod+Shift+E".action.quit;
+          "Mod+Shift+Slash".action.show-hotkey-overlay = 1;
+          "Mod+Shift+E".action.quit = 1;
           "Mod+Return".action.spawn = ["Kitty"];
           "Mod+D".action.spawn = ["wofi" "--show" "drun"];
         };
