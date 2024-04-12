19bb1b3952d5bf58b8b0781141bfa9432323a5f7
chinh4thepro <chinh4thepro@gmail.com>
2024-04-12 16:53:07 -0400

    a

--- home-manager/modules/niri.nix
+++ home-manager/modules/niri.nix
@@ -4,11 +4,12 @@
   niri,
   ...
 }: {
-    programs = {
-        niri = {
-            settings = {
-              outputs."eDP-1".scale=1.50;
-            };
-        };
+  programs = {
+    niri = {
+      package = pkgs.niri-stable;
+      settings = {
+        outputs."eDP-1".scale=1.50;
+      };
     };
+  };
 }
