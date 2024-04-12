6204b4a152cc370c1a35e6880ef6b8db1d7d8ce5
chinh4thepro <chinh4thepro@gmail.com>
2024-04-12 18:38:47 -0400

    erere

--- home-manager/modules/niri.nix
+++ home-manager/modules/niri.nix
@@ -17,8 +17,14 @@
 
         # Keybinds
         binds = with config.lib.niri.actions; {
+          # Key
           "Mod+Shift+Slash".action = show-hotkey-overlay;
+
+          # Basic Binds
           "Mod+Shift+E".action = quit;
+          "Mos+Shift+Q".action = close-window;
+
+          # Launch
           "Mod+Return".action = spawn "kitty";
           "Mod+D".action = spawn "wofi" "--show" "drun";
         };
