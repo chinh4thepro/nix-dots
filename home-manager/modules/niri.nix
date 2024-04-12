1d5c33269b25ed0b94aa8b941e2f142502d308e5
chinh4thepro <chinh4thepro@gmail.com>
2024-04-12 13:53:18 -0400

    niri?

--- home-manager/modules/niri.nix
+++ home-manager/modules/niri.nix
@@ -1,18 +1,15 @@
 {
   config,
   pkgs,
+  niri,
   ...
 }: {
-    nixpkgs = {
-      overlays = [
-        inputs.niri.overlays.niri
-      ];
-    };
-
     programs = {
         niri = {
             enable = true;
-            package = niri-stable;
+            settings = {
+              outputs."eDP-1".scale=1.50;
+            };
         };
     };
 }
